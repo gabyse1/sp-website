d1337b99759d2826b3e57136e4033b16705469ef
Gaby <gaby.es.sd@gmail.com>
2022-12-02 15:26:56 -0500

    Register the new User model to Django Admin.

--- app/appManageWSSP/admin.py
+++ app/appManageWSSP/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "first_name", "last_name", "last_login")
+
+admin.site.register(User, UserAdmin)
