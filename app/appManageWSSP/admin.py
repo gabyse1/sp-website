88b9ad4ff4b0cd3bf6738a5b49a254d00e4697f5
Gaby <gaby.es.sd@gmail.com>
2022-12-02 16:12:49 -0500

    Register all models in the django admin.

--- app/appManageWSSP/admin.py
+++ app/appManageWSSP/admin.py
@@ -5,4 +5,32 @@ from .models import *
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email", "first_name", "last_name", "last_login")
 
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name_es", "name_en", "modified", "user")
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "country", "modified", "user")
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Province)
+admin.site.register(District)
+admin.site.register(Contact)
+admin.site.register(MediaResource)
+admin.site.register(Video)
+admin.site.register(Imagen)
+admin.site.register(HtmlDesign)
+admin.site.register(Page)
+admin.site.register(Section)
+admin.site.register(ElementType)
+admin.site.register(Element)
+admin.site.register(DescriptiveArticle)
+admin.site.register(GraphicArticle)
+admin.site.register(HtmlArticle)
+admin.site.register(Slider)
+admin.site.register(SliderElement)
+admin.site.register(Author)
+admin.site.register(Publication)
+admin.site.register(PublicationArticle)
+admin.site.register(StyleSheet)
