c76b5c12aecfdbea4477ed9532425c017a05dcef
Gaby <gaby.es.sd@gmail.com>
2022-12-02 19:36:53 -0500

    Add the appManageWSSP and ckeditor_uploader urls to the sierraproductiva project main urls.

--- app/sierraproductiva/urls.py
+++ app/sierraproductiva/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/index/', admin.site.urls),
+    path('manage/', include('appManageWSSP.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
